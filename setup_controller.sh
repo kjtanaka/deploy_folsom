f24eaf1ff4a2d3f48b0e5478bbf14e7228ef15d2
Koji Tanaka <kj.tanaka@gmail.com>
2013-01-18 22:06:18 -0500

    update

--- setup_controller.sh
+++ setup_controller.sh
@@ -20,52 +20,19 @@ export DEBIAN_FRONTEND=noninteractive
 /usr/bin/aptitude -y update
 /usr/bin/aptitude -y upgrade
 /usr/bin/aptitude -y install \
-    ntp \
-    python-mysqldb \
-    python-memcache \
-    rabbitmq-server \
-    mysql-server \
-    memcached \
-    open-iscsi \
-    open-iscsi-utils \
-    kvm \
-    kvm-ipxe \
-    libvirt-bin \
-    bridge-utils \
-    python-libvirt \
-    keystone \
-    glance \
-    cinder-api \
-    cinder-scheduler \
-    cinder-volume \
-    python-cinderclient \
-    tgt \
-    nova-api \
-    nova-cert \
-    nova-compute \
-    nova-objectstore \
-    nova-network \
-    nova-scheduler \
-    nova-doc \
-    nova-console \
-    nova-consoleauth \
-    nova-novncproxy \
-    websockify \
-    novnc \
-    openstack-dashboard \
-    libapache2-mod-wsgi
-
-##############################################################################
-## Disable IPv6
-##############################################################################
-echo '127.0.0.1 localhost' > /etc/hosts
-echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
-
-##############################################################################
-## Configure memcached
-##############################################################################
-sed -i "s/127.0.0.1/$CONTROLLER/" /etc/memcached.conf
-service memcached restart
+	nova-api \
+	nova-cert \
+	nova-network \
+	nova-scheduler \
+	keystone \
+	glance \
+	nova-consoleauth \
+	nova-novncproxy \
+	novnc \
+	qpidd \
+	python-qpid \
+	mysql-server \
+	linux-image-extra-virtual
 
 ##############################################################################
 ## Make a script to start/stop all services
@@ -74,7 +41,7 @@ service memcached restart
 /bin/cat << EOF > openstack.sh
 #!/bin/bash
 
-NOVA="network scheduler cert consoleauth novncproxy api"
+NOVA="compute network scheduler cert consoleauth novncproxy api"
 GLANCE="registry api"
 KEYSTONE=""
 
@@ -112,126 +79,79 @@ EOF
 ## Modify configuration files of Nova, Glance and Keystone
 ##############################################################################
 
-for i in /etc/nova/nova.conf \
-    /etc/nova/api-paste.ini \
-	/etc/glance/glance-api.conf \
-	/etc/glance/glance-registry.conf \
-	/etc/keystone/keystone.conf \
-    /etc/cinder/cinder.conf \
-    /etc/openstack-dashboard/local_settings.py
-do
-	test -f $i.orig || /bin/cp $i $i.orig
-done
-
 /bin/cat << EOF > /etc/nova/nova.conf
 [DEFAULT]
 verbose=True
-allow_admin_api=True
-api_paste_config=/etc/nova/api-paste.ini
-instances_path=/var/lib/nova/instances
-compute_driver=libvirt.LibvirtDriver
-rootwrap_config=/etc/nova/rootwrap.conf
+debug=True
 multi_host=True
-send_arp_for_ha=True
-ec2_private_dns_show_ip=True
-start_guests_on_host_boot=True
-resume_guests_state_on_host_boot=True
 
-# LOGGING
+# PATH
 logdir=/var/log/nova
 state_path=/var/lib/nova
 lock_path=/var/lock/nova
 
-# NETWORK
-libvirt_use_virtio_for_bridges = True
-network_manager=nova.network.manager.FlatDHCPManager
-firewall_driver=nova.virt.libvirt.firewall.IptablesFirewallDriver
-dhcpbridge_flagfile=/etc/nova/nova.conf
-dhcpbridge=/usr/bin/nova-dhcpbridge
-public_interface=$PUBLIC_INTERFACE
-flat_interface=$FLAT_INTERFACE
-flat_network_bridge=br101
-fixed_range=$FIXED_RANGE
-#flat_network_dhcp_start=
-#network_size=255
-force_dhcp_release = True
-flat_injected=false
-use_ipv6=false
+# SCHEDULER
+compute_scheduler_driver=nova.scheduler.filter_scheduler.FilterScheduler
 
-# VNC
-vncserver_proxyclient_address=\$my_ip
-vncserver_listen=\$my_ip
-vnc_keymap=en
+# HYPERVISOR
+libvirt_type=kvm
+compute_driver=libvirt.LibvirtDriver
+libvirt_use_virtio_for_bridges=True
+connection_type=libvirt
+root_helper=sudo nova-rootwrap /etc/nova/rootwrap.conf
 
-#scheduler
-scheduler_driver=nova.scheduler.filter_scheduler.FilterScheduler
+# APIs
+ec2_private_dns_show_ip=True
+api_paste_config=/etc/nova/api-paste.ini
+enabled_apis=ec2,osapi_compute,metadata
 
-# OBJECT
-s3_host=$CONTROLLER
-use_cow_images=yes
+# DB
+sql_connection=mysql://openstack:$MYSQLPASS@$CONTROLLER/nova
+
+# AMQP
+rpc_backend=nova.openstack.common.rpc.impl_qpid
+qpid_hostname=$CONTROLLER
+qpid_username=nova@nova
+qpid_password=$QPID_PASS
+
+# KEYSTONE
+auth_strategy=keystone
 
 # GLANCE
 image_service=nova.image.glance.GlanceImageService
 glance_api_servers=$CONTROLLER:9292
 
-# RABBIT
-rabbit_host=$CONTROLLER
-rabbit_virtual_host=/nova
-rabbit_userid=nova
-rabbit_password=$RABBIT_PASS
+# NETWORK
+network_manager=nova.network.manager.FlatDHCPManager
+force_dhcp_release=True
+dhcpbridge_flagfile=/etc/nova/nova.conf
+dhcpbridge=/usr/bin/nova-dhcpbridge
+firewall_driver=nova.virt.libvirt.firewall.IptablesFirewallDriver
+public_interface=$PUBLIC_INTERFACE
+flat_network_bridge=br101
+flat_interface=$FLAT_INTERFACE
+fixed_range=$FIXED_RANGE
 
-# DATABASE
-sql_connection=mysql://openstack:$MYSQLPASS@$CONTROLLER/nova
+# NOVNC
+novncproxy_base_url=http://\$my_ip:6080/vnc_auto.html
+vncserver_proxyclient_address=\$my_ip
+vncserver_listen=\$my_ip
+vnc_keymap=en
 
-#use cinder
-enabled_apis=ec2,osapi_compute,metadata
-volume_api_class=nova.volume.cinder.API
-
-#keystone
-auth_strategy=keystone
-keystone_ec2_url=http://$CONTROLLER:5000/v2.0/ec2tokens
+# Cinder
+##volume_api_class=nova.volume.cinder.API
 EOF
 
-/bin/cat << EOF >> /etc/cinder/cinder.conf
-# LOGGING
-log_file=cinder.log
-log_dir=/var/log/cinder
-
-# OSAPI
-osapi_volume_extension = cinder.api.openstack.volume.contrib.standard_extensions
-
-# RABBIT
-rabbit_host=$CONTROLLER
-rabbit_virtual_host=/nova
-rabbit_userid=nova
-rabbit_password=$RABBIT_PASS
-
-# MYSQL
-sql_connection = mysql://openstack:$MYSQLPASS@$CONTROLLER/cinder
-EOF
-
-/bin/cat << EOF >> /etc/openstack-dashboard/local_settings.py
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'horizon',
-        'USER': 'openstack',
-        'PASSWORD': '$MYSQLPASS',
-        'HOST': '$CONTROPPER',
-        'default-character-set': 'utf8'
-    }
-}
-HORIZON_CONFIG = {
-    'dashboards': ('nova', 'syspanel', 'settings',),
-    'default_dashboard': 'nova',
-    'user_home': 'openstack_dashboard.views.user_home',
-}
-SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
-EOF
+for i in /etc/nova/api-paste.ini \
+	/etc/glance/glance-api.conf \
+	/etc/glance/glance-registry.conf \
+	/etc/keystone/keystone.conf
+do
+	test -f $i.orig || /bin/cp $i $i.orig
+done
 
 CONF=/etc/nova/api-paste.ini
 /bin/sed \
-    -e "^auth_host *=.*/auth_host = $CONTROLLER/" \
 	-e 's/%SERVICE_TENANT_NAME%/service/' \
 	-e 's/%SERVICE_USER%/nova/' \
 	-e "s/%SERVICE_PASSWORD%/$ADMIN_PASSWORD/" \
@@ -239,33 +159,22 @@ CONF=/etc/nova/api-paste.ini
 
 CONF=/etc/glance/glance-api.conf
 /bin/sed \
-    -e "^auth_host *=.*/auth_host = $CONTROLLER/" \
 	-e 's/%SERVICE_TENANT_NAME%/service/' \
 	-e 's/%SERVICE_USER%/glance/' \
 	-e "s/%SERVICE_PASSWORD%/$ADMIN_PASSWORD/" \
 	-e "s/^sql_connection *=.*/sql_connection = mysql:\/\/openstack:$MYSQLPASS@$CONTROLLER\/glance/" \
 	-e 's/^#* *config_file *=.*/config_file = \/etc\/glance\/glance-api-paste.ini/' \
-	-e 's/^#*flavor *=.*/flavor = keystone/' \
-    -e 's/^notifier_strategy *=.*/notifier_strategy = rabbit/' \
-    -e "s/^rabbit_host *=.*/rabbit_host = $CONTROLLER/" \
-    -e 's/^rabbit_userid *=.*/rabbit_userid = nova/' \
-    -e "s/^rabbit_password *=.*/rabbit_password = $RABBIT_PASS/" \
-    -e "s/^rabbit_virtual_host *=.*/rabbit_virtual_host = \/nova/" \
-    -e "s/127.0.0.1/$CONTROLLER/" \
-    -e "s/localhost/$CONTROLLER/" \
+	-e 's/^#*flavor *=.*/flavor=keystone/' \
 	$CONF.orig > $CONF
 
 CONF=/etc/glance/glance-registry.conf
 /bin/sed \
-    -e "^auth_host *=.*/auth_host = $CONTROLLER/" \
 	-e 's/%SERVICE_TENANT_NAME%/service/' \
 	-e 's/%SERVICE_USER%/glance/' \
 	-e "s/%SERVICE_PASSWORD%/$ADMIN_PASSWORD/" \
 	-e "s/^sql_connection *=.*/sql_connection = mysql:\/\/openstack:$MYSQLPASS@$CONTROLLER\/glance/" \
 	-e 's/^#* *config_file *=.*/config_file = \/etc\/glance\/glance-registry-paste.ini/' \
 	-e 's/^#*flavor *=.*/flavor=keystone/' \
-    -e "s/127.0.0.1/$CONTROLLER/" \
-    -e "s/localhost/$CONTROLLER/" \
 	$CONF.orig > $CONF
 
 CONF=/etc/keystone/keystone.conf
@@ -274,28 +183,41 @@ CONF=/etc/keystone/keystone.conf
 	-e "s/^#* *admin_token *=.*/admin_token = $ADMIN_PASSWORD/" \
 	$CONF.orig > $CONF
 
-CONF=/etc/cinder/api-paste.ini
-/bin/sed \
-    -e "^service_host *=.*/service_host = $CONTROLLER/" \
-    -e "^auth_host *=.*/auth_host = $CONTROLLER/" \
-	-e 's/%SERVICE_TENANT_NAME%/service/' \
-	-e 's/%SERVICE_USER%/cinder/' \
-	-e "s/%SERVICE_PASSWORD%/$ADMIN_PASSWORD/" \
-	$CONF.orig > $CONF
-
-for i in nova keystone glance cinder
+for i in nova keystone glance
 do
 	chown -R $i /etc/$i
 done
 
 ##############################################################################
-## Configure RabbitMQ
+## Create accounts of Nova, Glance and Cinder on Message Queuing System
 ##############################################################################
 
-rabbitmqctl add_vhost /nova
-rabbitmqctl add_user nova $RABBIT_PASS
-rabbitmqctl set_permissions -p /nova nova ".*" ".*" ".*"
-rabbitmqctl delete_user guest
+/usr/sbin/service qpidd stop
+
+PWDB=/etc/qpid/qpidd.sasldb
+test -f $PWDB.orig || /bin/mv $PWDB $PWDB.orig
+/bin/cp $PWDB.orig $PWDB
+/bin/chgrp qpidd $PWDB
+for i in nova glance cinder; do
+	echo $QPID_PASS | /usr/sbin/saslpasswd2 -c -p -f $PWDB -u $i $i
+done
+
+ACL=/etc/qpid/qpidd.acl
+test -f $ACL.orig || /bin/mv $ACL $ACL.orig
+/bin/cat << EOF > $ACL 
+# Group definitions
+group admin admin@QPID
+group openstack nova@nova cinder@cinder glance@glance
+
+# Admin is allowed to do everything
+acl allow admin all
+acl allow openstack all
+
+# Deny everything else by default
+acl deny all all
+EOF
+
+/usr/sbin/service qpidd start
 
 ##############################################################################
 ## Modify MySQL configuration
@@ -321,27 +243,22 @@ DROP DATABASE IF EXISTS keystone;
 DROP DATABASE IF EXISTS glance;
 DROP DATABASE IF EXISTS nova;
 DROP DATABASE IF EXISTS cinder;
-DROP DATABASE IF EXISTS horizon;
 CREATE DATABASE keystone;
 CREATE DATABASE glance;
 CREATE DATABASE nova;
 CREATE DATABASE cinder;
-CREATE DATABASE horizon;
 GRANT ALL ON keystone.* TO 'openstack'@'localhost' IDENTIFIED BY '$MYSQLPASS';
 GRANT ALL ON glance.*   TO 'openstack'@'localhost' IDENTIFIED BY '$MYSQLPASS';
 GRANT ALL ON nova.*     TO 'openstack'@'localhost' IDENTIFIED BY '$MYSQLPASS';
 GRANT ALL ON cinder.*   TO 'openstack'@'localhost' IDENTIFIED BY '$MYSQLPASS';
-GRANT ALL ON horizon.*   TO 'openstack'@'localhost' IDENTIFIED BY '$MYSQLPASS';
 GRANT ALL ON keystone.* TO 'openstack'@'$CONTROLLER' IDENTIFIED BY '$MYSQLPASS';
 GRANT ALL ON glance.*   TO 'openstack'@'$CONTROLLER' IDENTIFIED BY '$MYSQLPASS';
 GRANT ALL ON nova.*     TO 'openstack'@'$CONTROLLER' IDENTIFIED BY '$MYSQLPASS';
 GRANT ALL ON cinder.*   TO 'openstack'@'$CONTROLLER' IDENTIFIED BY '$MYSQLPASS';
-GRANT ALL ON horizon.*   TO 'openstack'@'$CONTROLLER' IDENTIFIED BY '$MYSQLPASS';
 GRANT ALL ON keystone.* TO 'openstack'@'$MYSQL_ACCESS'         IDENTIFIED BY '$MYSQLPASS';
 GRANT ALL ON glance.*   TO 'openstack'@'$MYSQL_ACCESS'         IDENTIFIED BY '$MYSQLPASS';
 GRANT ALL ON nova.*     TO 'openstack'@'$MYSQL_ACCESS'         IDENTIFIED BY '$MYSQLPASS';
 GRANT ALL ON cinder.*   TO 'openstack'@'$MYSQL_ACCESS'         IDENTIFIED BY '$MYSQLPASS';
-GRANT ALL ON horizon.*   TO 'openstack'@'$MYSQL_ACCESS'         IDENTIFIED BY '$MYSQLPASS';
 EOF
 
 ##############################################################################
@@ -364,41 +281,8 @@ sleep 5
 ## Create a sample data on Keystone
 ##############################################################################
 
-/bin/sed -e "s/localhost/$CONTROLLER/g" \
-         -e "s/127.0.0.1/$CONTROLLER/g" \
-         /usr/share/keystone/sample_data.sh > /tmp/sample_data.sh
+/bin/sed -e "s/localhost/$CONTROLLER/g" /usr/share/keystone/sample_data.sh > /tmp/sample_data.sh
 /bin/bash -x /tmp/sample_data.sh
-
-keystone endpoint-delete $(keystone endpoint-list | grep 8776 | awk '{print $2}')
-keystone service-delete $(keystone service-list | grep volume | awk '{print $2}')
-SERVICE_PASSWORD=$ADMIN_PASSWORD
-SERVICE_HOST=$CONTROLLER
-
-function get_id () {
-    echo `"$@" | awk '/ id / { print $4 }'`
-}
-ADMIN_ROLE=$(keystone role-list | grep " admin" | awk '{print $2}')
-SERVICE_TENANT=$(keystone tenant-list | grep service | awk '{print $2}')
-
-CINDER_USER=$(get_id keystone user-create \
-        --name=cinder \
-        --pass="$SERVICE_PASSWORD" \
-        --tenant_id $SERVICE_TENANT \
-        --email=cinder@example.com)
-keystone user-role-add \
-        --tenant_id $SERVICE_TENANT \
-        --user_id $CINDER_USER \
-        --role_id $ADMIN_ROLE
-CINDER_SERVICE=$(get_id keystone service-create \
-        --name=cinder \
-        --type=volume \
-        --description="Cinder Service")
-keystone endpoint-create \
-        --region RegionOne \
-        --service_id $CINDER_SERVICE \
-        --publicurl "http://$SERVICE_HOST:8776/v1/\$(tenant_id)s" \
-        --adminurl "http://$SERVICE_HOST:8776/v1/\$(tenant_id)s" \
-        --internalurl "http://$SERVICE_HOST:8776/v1/\$(tenant_id)s"
 
 ##############################################################################
 ## Create credentials
@@ -428,7 +312,7 @@ EOF
 	--label private \
 	--num_networks=1 \
 	--fixed_range_v4=$FIXED_RANGE \
-	--network_size=256
+	--network_size=256 \
     --multi_host=T
 
 CONF=/etc/rc.local
@@ -452,7 +336,6 @@ EOF
 ##############################################################################
 
 ./openstack.sh start
-service apache2 restart
 sleep 5
 
 ##############################################################################
